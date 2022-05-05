0bdee28576df5c992821fe665534a401d84312e3
aktivgo <aktivgo01062001@gmail.com>
2022-05-05 13:05:17 +0300

    Trying task 5

--- task_5/main.cpp
+++ task_5/main.cpp
@@ -6,67 +6,79 @@ std::vector<std::vector<int>> create_random_vectors(int, int);
 std::vector<int> create_random_vector(int);
 
 int main(int argc, char * argv[]) {
-    int n = 5, m = 5;
-
-//    std::cout << "Enter n: ";
-//    std::cin >> n;
-//
-//    std::cout << "\nEnter m: ";
-//    std::cin >> m;
-
     MPI_Status status;
 
-    std::vector<std::vector<int>> vectors = create_random_vectors(n, m);
-    std::vector<int> sums;
-
     MPI_Init(&argc, &argv);
-
-    int dest, source;
 
     int proc_num, proc_rank;
     MPI_Comm_size( MPI_COMM_WORLD, &proc_num);
     MPI_Comm_rank( MPI_COMM_WORLD, &proc_rank);
 
     if (proc_rank == 0) {
-        for (int i = 0; i < vectors.size(); i += proc_num - 1) {
-            for (int j = 1; j < proc_num; j++) {
-                dest = j;
-                source = j;
-                MPI_Send(vectors[i + j - 1].data(), m, MPI_INT, dest, 1,
-                         MPI_COMM_WORLD);
-                printf("%d send vector %d\n", proc_rank, i + j - 1);
-                int sum = 0;
-                MPI_Recv(&sum, 1, MPI_INT, source, 1,
-                         MPI_COMM_WORLD, &status);
-                printf("%d received sum %d\n", proc_rank, sum);
+        int n = 0, m = 0;
 
-                sums.push_back(sum);
+        std::cout << "Enter n: ";
+        std::cin >> n;
+
+        std::cout << "Enter m: ";
+        std::cin >> m;
+
+        std::vector<std::vector<int>> vectors = create_random_vectors(n, m);
+        std::vector<int> sums;
+
+        int countRows = n / (proc_num - 1);
+        int countRowsLast = countRows + n % (proc_num - 1);
+        for(int i = 1; i < proc_num; i++) {
+            if (i != proc_num - 1) {
+                MPI_Send(&countRows, 1, MPI_INT, i, 0,MPI_COMM_WORLD);
+            } else {
+                MPI_Send(&countRowsLast, 1, MPI_INT, i, 0,MPI_COMM_WORLD);
             }
+
+            MPI_Send(&m, 1, MPI_INT, i, 1,MPI_COMM_WORLD);
+        }
+
+        for(int i = 0; i < vectors.size(); i++) {
+            int targetThread = i % (proc_num - 1) + 1;
+
+            if (i >= countRowsLast - 1) {
+                targetThread = proc_num - 1;
+            }
+
+            MPI_Send(vectors[i].data(), m, MPI_INT, targetThread, targetThread,MPI_COMM_WORLD);
+            printf("%d send row to %d\n", proc_rank, targetThread);
+
+            int sum = 0;
+            MPI_Recv(&sum, 1, MPI_INT, targetThread, targetThread,MPI_COMM_WORLD, &status);
+            printf("%d received sum from %d\n", proc_rank, targetThread);
+
+            sums.push_back(sum);
         }
 
         int resultSum = 0;
         for (int sum : sums) {
             resultSum += sum;
         }
-        printf("%d result sum %d", proc_rank, resultSum);
+        printf("%d result sum %d\n", proc_rank, resultSum);
     } else {
-        dest = 0;
-        source = 0;
+        int n;
+        MPI_Recv(&n, 1, MPI_INT, 0, 0,MPI_COMM_WORLD, &status);
+        printf("received n=%d\n", n);
 
-        while(true) {
+        int m;
+        MPI_Recv(&m, 1, MPI_INT, 0, 1,MPI_COMM_WORLD, &status);
+
+        for(int i = 0; i < n; i++) {
             std::vector<int> vector(m);
-            MPI_Recv(vector.data(), m, MPI_INT, source, 1,
-                     MPI_COMM_WORLD, &status);
-            printf("%d received vector\n", proc_rank);
+
+            MPI_Recv(vector.data(), m, MPI_INT, 0, proc_rank, MPI_COMM_WORLD, &status);
 
             int sum = 0;
-            for (int i : vector) {
-                sum += i;
+            for (int item : vector) {
+                sum += item;
             }
 
-            MPI_Send(&sum, 1, MPI_INT, dest, 1,
-                     MPI_COMM_WORLD);
-            printf("%d send sum %d\n", proc_rank, sum);
+            MPI_Send(&sum, 1, MPI_INT, 0, proc_rank,MPI_COMM_WORLD);
         }
     }
 
